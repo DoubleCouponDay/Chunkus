d7da046dd8f9431f71a9fe17f0eec48d7b31a9c0
Bobscorn <qwertster0@gmail.com>
2020-12-24 17:00:51 +1300

    Auto stash before merge of "master" and "origin/master"

---
+++ discord-v/tests/discordtests/handlers.rs
@@ -0,0 +1,81 @@
+use serenity::client::{
+    Context, EventHandler
+};
+use serenity::{
+    async_trait,
+    model::{
+        prelude::Message
+    },
+};
+use std::sync::{
+    Mutex, Arc
+};
+
+pub static MESSAGE_CONTENT: &'static str = "testy boi";
+
+pub struct ReceiveEmbedMessageHandler
+{
+    pub message_received_mutex: Arc<Mutex<bool>>,
+}
+pub struct ReceiveMessageHandler;
+pub struct ReceiveImageEmbedMessageHandler;
+
+#[async_trait]
+impl EventHandler for ReceiveEmbedMessageHandler
+{
+    async fn message(&self, ctx: Context, msg: Message)
+    {
+        if msg.content == MESSAGE_CONTENT && msg.embeds.len() > 0
+        {
+            println!("Found test message");
+
+            *self.message_received_mutex.lock().unwrap() = true;
+        }
+        else
+        {
+            println!("Found non-test message");
+        }
+    }
+}
+
+#[async_trait]
+impl EventHandler for ReceiveImageEmbedMessageHandler
+{
+    async fn message(&self, ctx: Context, msg: Message)
+    {
+        if msg.content == MESSAGE_CONTENT && msg.embeds.len() > 0
+        {
+            match &msg.embeds[0].image
+            {
+                Some(img) => {
+                    println!("Found special test message");
+                
+                }
+                None => {}
+            }
+        }
+
+        else
+        {
+            println!("Found non-test message");
+        }
+    }
+}
+
+#[async_trait]
+impl EventHandler for ReceiveMessageHandler
+{
+    async fn message(&self, ctx: Context, msg: Message)
+    {
+        if msg.content == MESSAGE_CONTENT
+        {
+            println!("Found test message");
+        
+        }
+
+        else
+        {
+            println!("Found non-test message");
+        }
+    }
+}
