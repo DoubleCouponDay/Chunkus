452b9f7206027ac76e5c881bf94a433a44671b66
Bobscorn <qwertster0@gmail.com>
2021-01-30 20:33:48 +1300

    allow rust to link release binary

--- discord-v/build.rs
+++ discord-v/build.rs
@@ -1,25 +1,46 @@
 #![allow(unused_imports)]
 fn main() {
     let path = std::env::current_dir().unwrap();
+    let do_release = std::env::var("RELEASE").unwrap_or(String::from("false")).parse::<bool>().unwrap_or(false);
 
     if let Some(currentdir) = path.as_path().to_str() {
         println!("current directory: {}", currentdir);
 
         //detect the C part
+        let windowsfound_shared;
+        let linuxfound_shared;
+        let windows_found_static;
+        if do_release
+        {
+            windowsfound_shared = std::fs::copy(
+                "../build/windows/x64/release/vec.dll",
+                "./vec.dll"
+            );
+            linuxfound_shared = std::fs::copy(
+                "../build/linux/x86_64/release/libvec.a",
+                "./vec.so",
+            );
+            windows_found_static = std::fs::copy(
+                "../build/windows/x64/release/vec.lib",
+                "./vec.lib"
+            );
+        }
+        else
+        {
+            windowsfound_shared = std::fs::copy(
+                "../build/windows/x64/debug/vec.dll",
+                "./vec.dll"
+            );
+            linuxfound_shared = std::fs::copy(
+                "../build/linux/x86_64/debug/libvec.a",
+                "./vec.so",
+            );
 
-        let windowsfound_shared = std::fs::copy(
-            "../build/windows/x64/debug/vec.dll",
-            "./vec.dll"
-        );
-        let linuxfound_shared = std::fs::copy(
-            "../build/linux/x86_64/debug/libvec.a",
-            "./vec.so",
-        );
-
-        let windows_found_static = std::fs::copy(
-            "../build/windows/x64/debug/vec.lib",
-            "./vec.lib"
-        );
+            windows_found_static = std::fs::copy(
+                "../build/windows/x64/debug/vec.lib",
+                "./vec.lib"
+            );
+        }
 
         if windowsfound_shared.is_err() && linuxfound_shared.is_err() && windows_found_static.is_err() {
             println!("C part not found on either OS.");
