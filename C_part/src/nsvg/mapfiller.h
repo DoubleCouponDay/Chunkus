bf55129d14e7a40103ba40ddbf3d2f73ccb39c3f
doublecouponday <mail@samueljenks.me>
2021-02-11 14:06:57 +1300

    fix: forgot part of refactor

--- C_part/src/nsvg/mapfiller.h
+++ C_part/src/nsvg/mapfiller.h
@@ -3,4 +3,3 @@
 #include "../chunkmap.h"
 
 void fill_chunkmap(chunkmap* map, vectorize_options* options);
-void iterate_chunk_shapes(chunkmap* map, NSVGimage* output);
---
+++ C_part/src/nsvg/mapparser.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <nanosvg.h>
+#include "../chunkmap.h"
+
+void iterate_chunk_shapes(chunkmap* map, NSVGimage* output);
