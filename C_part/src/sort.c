9d90be98e2d3a5d5f2537678143cc0e8026b2765
doublecouponday <mail@samueljenks.me>
2021-02-17 23:28:41 +1300

    fix: stack overflow on large images

--- C_part/src/sort.c
+++ C_part/src/sort.c
@@ -15,80 +15,102 @@ enum {
     ADJACENT_COUNT = 9
 };
 
+vector2 calculate_2d_diff(pixelchunk* subject, pixelchunk* previous) {
+    int x_diff = subject->location.x - previous->location.x;
+    int y_diff = subject->location.y - previous->location.y;
+    vector2 diff = { x_diff, y_diff };
+    return diff;
+}
+
+float calculate_angle_between(pixelchunk* eligible, pixelchunk* subject, pixelchunk* previous) {
+    int eligible_x_diff = eligible->location.x - subject->location.x;
+    int eligible_y_diff = eligible->location.y - subject->location.y;
+    vector2 subject_to_eligible = { eligible_x_diff, eligible_y_diff };
+    vector2 diff = calculate_2d_diff(subject, previous);
+    float angle = vec_angle_between(diff, subject_to_eligible);
+    return angle;
+}
+
 void sort_item(pixelchunk** array, pixelchunk* current, unsigned long i, unsigned long next, unsigned long length) {
     pixelchunk* inbetween = array[next];
     array[next] = current;
     array[i] = inbetween;
-    bubble_sort(array, next, length);
+    //bubble_sort(array, next, length);
+}
+
+void dont_skip_corners(pixelchunk** array, unsigned long eligiblesubjects[ADJACENT_COUNT], pixelchunk* subject, pixelchunk* previous, 
+                        unsigned long eligible_count, unsigned long next, unsigned long length) {
+
+    float smallest_angle = M_PI * 2.f; // set to largest possible radian to begin with
+    pixelchunk* most_eligible = NULL;
+    unsigned long most_eligible_index = 0;
+
+    for (unsigned long q = 0; q < eligible_count; ++q) {
+        pixelchunk* eligible = array[eligiblesubjects[q]];
+        float angle = calculate_angle_between(eligible, subject, previous);
+
+        if (angle < smallest_angle)
+        {
+            smallest_angle = angle;
+            most_eligible = eligible;
+            most_eligible_index = eligiblesubjects[q];
+        }
+    }
+
+    if (most_eligible)
+    {
+        sort_item(array, most_eligible, most_eligible_index, next, length);
+    }
+
+    else
+    {
+        return;
+    }
 }
 
 void bubble_sort(pixelchunk** array, unsigned long start, unsigned long length) {
-    unsigned long eligiblesubjects[ADJACENT_COUNT] = {0};
-    pixelchunk* subject = array[start];
-    unsigned long next = start + 1;
-    unsigned long eligible_count = 0;
-    
-    pixelchunk* previous = (start ? array[start - 1] : NULL);
+    bool allsorted = false;
 
-    for(unsigned long i = next; i < length; ++i) {
-        pixelchunk* current = array[i];
-        int abs_x_diff = abs(subject->location.x - current->location.x);
-        int abs_y_diff = abs(subject->location.y - current->location.y);
-        bool xisclose = abs_x_diff <= 1;
-        bool yisclose = abs_y_diff <= 1;
+    while(allsorted == false) {        
+        unsigned long next = start + 1;
 
-        if((xisclose && yisclose)) {
-            if(previous == NULL) {
-                sort_item(array, current, i, next, length);
-                return;
-            }
-
-            else if(eligible_count == ADJACENT_COUNT) {
-                DEBUG("adjacent chunks are larger than known size!\n");
-                setError(ASSUMPTION_WRONG);
-                return;
-            }
-            eligiblesubjects[eligible_count] = i;
-            ++eligible_count;            
+        if(next >= length) {
+            allsorted = true;
+            return;
         }
+        unsigned long eligiblesubjects[ADJACENT_COUNT] = {0};
+        pixelchunk* subject = array[start];
+        unsigned long eligible_count = 0;
+        pixelchunk* previous = (start ? array[start - 1] : NULL);
 
-        if(i == length - 1) 
-        {            
-            int x_diff = subject->location.x - previous->location.x;
-            int y_diff = subject->location.y - previous->location.y;
-            vector2 diff = { x_diff, y_diff };
+        for(unsigned long i = next; i < length; ++i) {
+            pixelchunk* current = array[i];
+            int abs_x_diff = abs(subject->location.x - current->location.x);
+            int abs_y_diff = abs(subject->location.y - current->location.y);
+            bool xisclose = abs_x_diff <= 1;
+            bool yisclose = abs_y_diff <= 1;
 
-            float smallest_angle = M_PI * 2.f; // Largest angle physically possible
-            pixelchunk* most_eligible = NULL;
-            unsigned long most_eligible_index = 0;
-
-            for (unsigned long q = 0; q < eligible_count; ++q) {
-                pixelchunk* eligible = array[eligiblesubjects[q]];
-                int eligible_x_diff = eligible->location.x - subject->location.x;
-                int eligible_y_diff = eligible->location.y - subject->location.y;
-                vector2 subject_to_eligible = { eligible_x_diff, eligible_y_diff };
-
-                float angle = vec_angle_between(diff, subject_to_eligible);
-
-                if (angle < smallest_angle)
-                {
-                    smallest_angle = angle;
-                    most_eligible = eligible;
-                    most_eligible_index = eligiblesubjects[q];
+            if((xisclose && yisclose)) {
+                if(previous == NULL) {
+                    sort_item(array, current, i, next, length);
+                    break;
                 }
+
+                else if(eligible_count == ADJACENT_COUNT) {
+                    DEBUG("adjacent chunks are larger than known size!\n");
+                    setError(ASSUMPTION_WRONG);
+                    return;
+                }
+                eligiblesubjects[eligible_count] = i;
+                ++eligible_count;            
             }
 
-            if (most_eligible)
-            {
-                sort_item(array, most_eligible, most_eligible_index, next, length);
-            }
-
-            else
-            {
-                DEBUG("No Eligible chunks found adjacent\n");
-                return;
+            if(i == length - 1) 
+            {            
+                dont_skip_corners(array, eligiblesubjects, subject, previous, eligible_count, next, length);
             }
         }
+        ++start;
     }
 }
 
