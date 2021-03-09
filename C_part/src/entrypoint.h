1d46342e318851ebb167f59a7f10d62264c04d5d
dcdgo <haveachillday@gmail.com>
2021-03-10 10:49:23 +1300

    refactor: vectorize options to new location

--- C_part/src/entrypoint.h
+++ C_part/src/entrypoint.h
@@ -2,6 +2,13 @@
 
 extern const int NUM_COLOURS;
 
+typedef struct {
+    char* file_path;
+    int chunk_size;
+    float shape_colour_threshhold;
+    int num_colours;
+} vectorize_options;
+
 int entrypoint(int argc, char* argv[]);
 int set_algorithm(char* algo);
 
