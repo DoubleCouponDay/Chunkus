fd172ace7e945651f19a59c8c772b0ca16612bb3
Bobstuna <qwertster0@gmail.com>
2022-05-23 21:13:30 +1200

    rm rust test

--- Rust_part/tests/coretests.rs
+++ Rust_part/tests/coretests.rs
@@ -9,19 +9,6 @@ mod tests {
     use tokio;
 
     #[tokio::test]
-    async fn algorithm_can_be_changed() {
-        let algo_result = set_algorithm("bobsweep");
-        let mut midstep: String = algo_result.to_string();
-        let tripstep: &str = midstep.as_mut_str();
-
-        match algo_result {
-            FfiResult::SuccessCode => println!("{}", tripstep), 
-            _ => panic!("{}", tripstep) 
-        };
-        ()
-    }
-    
-    #[tokio::test]
     async fn invoke() {
         let input_file = String::from("test.png");
         let output_file = String::from("output.png");
