afd6bbfd5680340bf53c9716a02cae4799d36d0b
Bobstuna <qwertster0@gmail.com>
2022-08-06 13:13:38 +1200

    fix color conversion

--- Cpp_part/src/color.cpp
+++ Cpp_part/src/color.cpp
@@ -1,5 +1,7 @@
 #include "color.h"
 
+#include <cmath>
+
 Color32 Convert8to32(Color8 color)
 {
     return Color32{ (float)color.R / 255.f, (float)color.G / 255.f, (float)color.B / 255.f };
@@ -7,5 +9,8 @@ Color32 Convert8to32(Color8 color)
 
 Color8 Convert32to8(Color32 color)
 {
-    return Color8{ (unsigned char)(color.R * 255.f), (unsigned char)(color.B * 255.f), (unsigned char)(color.B * 2.f) };
+    int r = roundf(color.R * 255.f);
+    int g = roundf(color.G * 255.f);
+    int b = roundf(color.B * 255.f);
+    return Color8{ (unsigned char)r, (unsigned char)g, (unsigned char)b };
 }
