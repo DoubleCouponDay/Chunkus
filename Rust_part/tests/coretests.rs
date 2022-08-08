625d1a185b572f02354d110c6ef29deddbbfca71
doublecouponday <mail@samueljenks.me>
2022-08-08 19:23:43 +1200

    fix: rust test

--- Rust_part/tests/coretests.rs
+++ Rust_part/tests/coretests.rs
@@ -11,14 +11,13 @@ mod tests {
     #[tokio::test]
     async fn invoke() {
         let input_file = String::from("test.png");
-        let output_file = String::from("output.png");
 
         let input = ParsedOptions{
             chunksize: String::from("1"), threshold: String::from("5"),
             numcolours: String::from("256"), 
             shouldcrash: String::from("false")
         };
-        let algo_result = do_vectorize(&input_file, &output_file, input);
+        let algo_result = do_vectorize(&input_file, input);
 
         if algo_result != FfiResult::SuccessCode
         {
