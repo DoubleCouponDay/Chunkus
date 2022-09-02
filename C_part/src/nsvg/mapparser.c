10291b7a599287fc2305ac65f5f858293fd93952
Samuel Jenks <mail@samueljenks.me>
2022-09-02 12:53:20 +1200

    refactor: clearer name

--- C_part/src/nsvg/mapparser.c
+++ C_part/src/nsvg/mapparser.c
@@ -26,8 +26,8 @@ typedef struct
 } parsing_data;
 
 //assumes first path and first shape are given
-void add_to_path(pixelchunk* chunk, parsing_data* shape_data) {
-    NSVGshape* current = shape_data->output->shapes;
+void add_to_path(pixelchunk* chunk, parsing_data* data) {
+    NSVGshape* current = data->output->shapes;
     NSVGpath* currentpath = current->paths;
 
     // Check if current nsvg path has either points yet to be filled
@@ -37,8 +37,8 @@ void add_to_path(pixelchunk* chunk, parsing_data* shape_data) {
         currentpath->pts[0] = chunk->border_location.x; //x1
         currentpath->pts[1] = chunk->border_location.y; //y1
 
-        shape_data->shapescolour = calloc(1, sizeof(NSVGpaint));
-        NSVGpaint* fill = shape_data->shapescolour;
+        data->shapescolour = calloc(1, sizeof(NSVGpaint));
+        NSVGpaint* fill = data->shapescolour;
         fill->type = NSVG_PAINT_COLOR;
 
         fill->color = NSVG_RGB(
@@ -64,7 +64,7 @@ void add_to_path(pixelchunk* chunk, parsing_data* shape_data) {
     };
 
     NSVGpath* nextsegment = create_path(
-        shape_data->map->input, 
+        data->map->input, 
         previous_coord,
         chunk->border_location
     );
