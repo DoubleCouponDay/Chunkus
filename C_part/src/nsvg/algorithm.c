c1c838fcf1caf2a1b7433a453b1afe74ddb960f0
Bobstuna <qwertster0@gmail.com>
2022-09-06 12:56:51 +1200

    fix not setting map->first_shape

--- C_part/src/nsvg/algorithm.c
+++ C_part/src/nsvg/algorithm.c
@@ -132,10 +132,13 @@ chunkshape* add_new_shape(Quadrant* quadrant) {
     new->boundaries_length = 0;
     new->chunks_amount = 0;
 
-    if(quadrant->map->shape_list != NULL) {
+    if(quadrant->map->shape_list != NULL)
         quadrant->map->shape_list->next = new; //links to the previous last item
-    }
+    else
+        quadrant->map->first_shape = new; //if there was no previous item, this is the first item
+        
     quadrant->map->shape_list = new; //sets the linked list to last item
+
     ++quadrant->map->shape_count;
     return new;
 }
