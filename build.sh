9d43c0a48b9909aecdf22920db96481bbd1df2c0
doublecouponday <mail@samueljenks.me>
2022-09-24 10:59:45 +1200

    fix: cant make build folder

--- build.sh
+++ build.sh
@@ -53,6 +53,7 @@ cmake --install build --prefix install
 
 echo "building Chunkus..."
 cd ../Chunkus
+sudo rm -rf build
 mkdir build
 sudo cmake -B build -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 sudo cmake --build build -j4
