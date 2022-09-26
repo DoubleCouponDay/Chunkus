4763cea9ebbbcee21af2e2625f3d8bc1fc2266b2
doublecouponday <mail@samueljenks.me>
2022-09-26 13:42:58 +1300

    chore: print cwd in build.rs

--- Rust_part/build.rs
+++ Rust_part/build.rs
@@ -20,16 +20,17 @@ fn main() {
     stop_if_unknown_os();
     let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let cwd = Path::new(&dir);
+    println!("cwd: {}", cwd.display());
 
     println!("cargo:rustc-link-search=native={}", cwd.display());
-    println!("cargo:rustc-link-search=native={}/../install/lib", cwd.display());
+    println!("cargo:rustc-link-search=native=/Chunkus/install/lib");
 
     println!("cargo:rerun-if-changed=build.rs");
 }
 
 fn stop_if_unknown_os() {
-    if cfg!(target_os = "linux") == false
-        && cfg!(target_os = "windows") == false {
+    if cfg!(target_os = "linux") == false &&
+        cfg!(target_os = "windows") == false {
         panic!("This operating system is not supported.");
     }
 }
