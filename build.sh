43876a98c2c7696d1b1ef0d9b0ee1a70c18d07ed
doublecouponday <mail@samueljenks.me>
2022-09-29 21:01:27 +1300

    chore: include rust in build scripts

--- build.sh
+++ build.sh
@@ -57,3 +57,6 @@ cd ../Chunkus
 sudo cmake -B build -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 sudo cmake --build build -j4
 sudo cmake --install build --prefix install
+
+cd ./Rust_part
+cargo build
