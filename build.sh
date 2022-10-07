df16ae07cc8e3cdd1ae889e6f6a9356c2704704c
doublecouponday <mail@samueljenks.me>
2022-10-07 19:33:29 +1300

    fix: elevated privileges in build script

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-sudo apt update -y
-sudo apt install git cmake build-essential mesa-common-dev libxi-dev libx11-dev libgl1-mesa-dev libglu1-mesa-dev -y
 
 cd ../
 git clone https://github.com/madler/zlib.git
@@ -12,38 +10,38 @@ git clone https://github.com/winlibs/libjpeg.git
 echo "Building zlib..."
 cd zlib
 git checkout v1.2.12
-sudo cmake -B build -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX="install"
-sudo cmake --build build -j4
-sudo cmake --install build
+cmake -B build -G "Unix Makefiles" -D CMAKE_INSTALL_PREFIX="install"
+cmake --build build -j4
+cmake --install build
 
 echo "Building libpng..."
 cd ../libpng
 git checkout v1.6.35
-sudo cmake -B build -G "Unix Makefiles" -D ZLIB_ROOT="../zlib/install"
-sudo cmake --build build -j4
-sudo cmake --install build --prefix png-install
+cmake -B build -G "Unix Makefiles" -D ZLIB_ROOT="../zlib/install"
+cmake --build build -j4
+cmake --install build --prefix png-install
 
 echo "Building nanosvg..."
 cd ../nanosvg
 git checkout 3bcdf2f3cdc1bf9197c2dce81368bfc6f99205a7
-sudo cmake -B build -G "Unix Makefiles"
-sudo cmake --build build -j4
-sudo cmake --install build --prefix install
+cmake -B build -G "Unix Makefiles"
+cmake --build build -j4
+cmake --install build --prefix install
 
 echo "Building freeglut..."
 cd ../freeglut
 git checkout v3.2.2
-sudo apt install libxi-dev -y
-sudo cmake -B build -G "Unix Makefiles"
-sudo cmake --build build -j4
-sudo cmake --install build --prefix install
+apt install libxi-dev -y
+cmake -B build -G "Unix Makefiles"
+cmake --build build -j4
+cmake --install build --prefix install
 
 echo "Building lunasvg..."
 cd ../lunasvg
 git checkout v2.3.1
-sudo cmake -B build -G "Unix Makefiles"
-sudo cmake --build build -j4
-sudo cmake --install build --prefix install
+cmake -B build -G "Unix Makefiles"
+cmake --build build -j4
+cmake --install build --prefix install
 
 echo "Building libjpeg-turbo..."
 cd ../libjpeg
@@ -54,9 +52,9 @@ cmake --install build --prefix install
 
 echo "building Chunkus..."
 cd ../Chunkus
-sudo cmake -B build -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
-sudo cmake --build build -j4
-sudo cmake --install build --prefix install
+cmake -B build -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
+cmake --build build -j4
+cmake --install build --prefix install
 
 cd ./Rust_part
 cargo build
