c790e1316365727e75d73382deb2e4517f77dd5b
doublecouponday <mail@samueljenks.me>
2022-10-07 19:47:32 +1300

    fix: build script needed elevated privileges

--- build.sh
+++ build.sh
@@ -31,7 +31,6 @@ cmake --install build --prefix install
 echo "Building freeglut..."
 cd ../freeglut
 git checkout v3.2.2
-apt install libxi-dev -y
 cmake -B build -G "Unix Makefiles"
 cmake --build build -j4
 cmake --install build --prefix install
