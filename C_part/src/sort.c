75d82f0bd2609036d3001a5937ce70a3b7f772dc
doublecouponday <mail@samueljenks.me>
2022-10-24 10:53:54 +1300

    step: sort state machine

--- C_part/src/sort.c
+++ C_part/src/sort.c
@@ -52,85 +52,119 @@ void sort_boundary_chunk(Quadrant* quadrant, chunkshape* shape, pixelchunk_list*
     else if(current == shape->first_boundary && shape->boundaries_length > 0) { //dont try to sort the first chunk
         LOG_ERR("current boundary chunk cannot be first! this is a finite linked list.");
         setError(ASSUMPTION_WRONG);
-        return;
     }
 
     else if(current == shape->boundaries) { //dont try to sort the last chunk
         LOG_ERR("current boundary chunk cannot be last! this is a finite linked list.");
         setError(ASSUMPTION_WRONG);
-        return;
     }
 
     else { //shapes boundary on next scanline starts back to front away from first or last boundary chunk
-
-    /**
-     * problem
-	I am trying to sort boundary chunks immediately after they get flagged as a boundary but it is not working
-
-    guess
-        the boundary chunks are sometimes not adjacent to the first or last boundary chunks
-        
-    try
-        build the boundary backwards until it hits the last chunk
-        
-        left, top left, top, top right chunks can never be on the boundary
-        
-        try to find two adajacent chunks that are boundaries
-        
-        if bot left, bot, bot right, right are possible boundaries
-        
-        assume there to be at most 3 boundaries
-        
-        if less than 2 found, throw error
-        
-        if 2 or 3 found
-            if this is the first loop, save the chunk furthest away from the last chunk for later
-            
-            if 2 found
-                set furthest away chunk to next
-                
-                set closest chunks next to current chunk
-                
-            if 3 found
-                set furthest away's chunk's next to second furthest away
-                
-                set second furthest away chunk to next
-                
-                set closest chunk's next to current
-                
-        loop until connected with adjacent
-
-        any chunk added to the boundary should be immediately zipped
-        
-        disable zipping in algorithm.c if sort.c sort_boundary_chunk returned true
-
-     * 
-     */
-        LOG_INFO("%s: building boundary backwards from current: %dx,%dy to last: %dx:%dy", quadrant->name, current->chunk_p->location.x, current->chunk_p->location.y, shape->boundaries->chunk_p->location.x, shape->boundaries->chunk_p->location.y);
+        LOG_INFO("current chunk is not adjacent to first or last chunk");
         int current_x = current->chunk_p->location.x;
         int current_y = current->chunk_p->location.y;
         int last_x = shape->boundaries->chunk_p->location.x;
         int last_y = shape->boundaries->chunk_p->location.y;
-        pixelchunk_list* current_work = current;
 
-        while(current_x <= last_x + 2) { //connect boundary chunks backwards until chunks have gone too far
-            pixelchunk* bot_left = get_at(quadrant, current_x - 1, current_y + 1);
-            pixelchunk* bot = get_at(quadrant, current_x, current_y + 1);
-            pixelchunk* bot_right = get_at(quadrant, current_x + 1, current_y + 1);            
-            pixelchunk* right = get_at(quadrant, current_x + 1, current_y);
-            pixelchunk* top_right = get_at(quadrant, current_x + 1, current_y - 1);
- 
-            bool top_similar = colours_are_similar(current_work->chunk_p->average_colour, top_right->average_colour, quadrant->options->threshold);
-            bool mid_similar = colours_are_similar(current_work->chunk_p->average_colour, right->average_colour, quadrant->options->threshold);
-            bool bot_similar = colours_are_similar(current_work->chunk_p->average_colour, bot_right->average_colour, quadrant->options->threshold);
+        //left, top_left, top, top_right can never be on the boundary
+        //bot_left, bot, bot_right, right are possible boundaries
+        pixelchunk* bot_left = get_at(quadrant, current_x - 1, current_y + 1);
+        pixelchunk* bot = get_at(quadrant, current_x, current_y + 1);
+        pixelchunk* bot_right = get_at(quadrant, current_x + 1, current_y + 1);            
+        pixelchunk* right = get_at(quadrant, current_x + 1, current_y);
 
-            
-            current_x = current_work->chunk_p->location.x;
-            current_y = current_work->chunk_p->location.y;
+        bool botleft_similar = colours_are_similar(current->chunk_p->average_colour, bot_left->average_colour, quadrant->options->threshold);
+
+        bool bot_similar = colours_are_similar(current->chunk_p->average_colour, bot->average_colour, quadrant->options->threshold);
+
+        bool botright_similar = colours_are_similar(current->chunk_p->average_colour, bot_right->average_colour, quadrant->options->threshold);
+        
+        bool right_similar = colours_are_similar(current->chunk_p->average_colour, right->average_colour, quadrant->options->threshold);
+
+        //XXXX
+        if(botleft_similar && bot_similar && botright_similar && right_similar) {
+            LOG_ERR("some chunks should be in the other shape but are not.");
+            setError(ASSUMPTION_WRONG);
         }
 
-        LOG_ERR("current chunk is not adjacent to first or last chunk in boundary checks!");
-        setError(ASSUMPTION_WRONG);
-        return;
+        //XXXO
+        else if(botleft_similar && bot_similar && botright_similar && !right_similar) {
+            
+        }
+        
+        //XXOX
+        else if(botleft_similar && bot_similar && !botright_similar && right_similar) {
+
+        }
+        
+        //XXOO
+        else if(botleft_similar && bot_similar && !botright_similar && !right_similar) {
+
+        }
+
+        //XOXX
+        else if(botleft_similar && !bot_similar && botright_similar && right_similar) {
+            
+        }
+
+        //XOXO
+        else if(botleft_similar && !bot_similar && botright_similar && !right_similar) {
+
+        }
+
+        //XOOX
+        else if(botleft_similar && !bot_similar && !botright_similar && right_similar) {
+
+        }
+
+        //XOOO
+        else if(botleft_similar && !bot_similar && !botright_similar && !right_similar) {
+            LOG_ERR("only botleft is similar!");
+            setError(ASSUMPTION_WRONG);
+        }
+
+        //OXXX
+        else if(!botleft_similar && bot_similar && botright_similar && right_similar) {
+
+        }
+
+        //OXXO
+        else if(!botleft_similar && bot_similar && botright_similar && !right_similar) {
+
+        }
+
+        //OXOX
+        else if(!botleft_similar && bot_similar && !botright_similar && right_similar) {
+
+        }
+
+        //OXOO
+        else if(!botleft_similar && bot_similar && !botright_similar && !right_similar) {
+            LOG_ERR("only bot is similar!");
+            setError(ASSUMPTION_WRONG);
+        }
+
+        //OOXX
+        else if(!botleft_similar && !bot_similar && botright_similar && right_similar) {
+
+        }
+
+        //OOXO
+        else if(!botleft_similar && !bot_similar && botright_similar && !right_similar) {
+            LOG_ERR("only botright is similar!");
+            setError(ASSUMPTION_WRONG);
+        }
+
+        //OOOX
+        else if(!botleft_similar && !bot_similar && !botright_similar && right_similar) {
+            LOG_ERR("only right is similar");
+            setError(ASSUMPTION_WRONG);
+        }
+
+        //OOOO
+        else {
+            LOG_ERR("none are similar!");
+            setError(ASSUMPTION_WRONG);
+        }
     }
 }
