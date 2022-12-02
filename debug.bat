88d775d08712de2af687addeee78ecb5116875db
doublecouponday <mail@samueljenks.me>
2022-12-02 22:07:55 +1300

    fix: slow debugging

--- debug.bat
+++ debug.bat
@@ -2,8 +2,8 @@ echo on
 
 echo "building Chunkus..."
 cd ../Chunkus
-cmake -B build -G "Ninja" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
-cmake --build build -j4 --config Debug
+cmake -B build -G "Ninja" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="RelWithDebInfo" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
+cmake --build build -j4 --config RelWithDebInfo
 cmake --install build --prefix install
 
 cd ./Rust_part
