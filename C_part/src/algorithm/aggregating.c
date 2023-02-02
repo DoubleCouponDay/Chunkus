9f516b4a8d64988a101a0282df9840abb07b1606
sam <mail@samueljenks.me>
2023-02-02 16:05:19 +1300

    fix: build script wrong generator

---
+++ C_part/src/algorithm/aggregating.c
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "aggregating.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "../chunkmap.h"
+
+void OR_64(pixelchunk** compare1, pixelchunk** compare2, pixelchunk** output, int width, int height) {
+    for(int x = 0; x < width; ++x) {
+        for(int y = 0; y < height; ++y) {
+            
+        }
+    }
+}
