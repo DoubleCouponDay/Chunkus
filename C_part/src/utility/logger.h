85ff90043f8389974aa6f7232059e5cd2590f726
Bobscorn <qwertster0@gmail.com>
2023-02-05 23:45:54 +1300

    fix msvc logger errors

--- C_part/src/utility/logger.h
+++ C_part/src/utility/logger.h
@@ -1,17 +1,15 @@
-#ifndef LOGGER_H
-#define LOGGER_H
+#pragma once
 
 void clear_logfile();
 void logger(const char* tag, const char* message, ...);
 
 #ifdef _MSC_VER
-#define LOG_INFO(fmt, ...) logger("INFO", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LOG_WARN(fmt, ...) logger("WARNING", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__)
-#define LOG_ERR(fmt, ...) logger("ERROR", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, __VA_ARGS__)
+
+#define LOG_INFO(fmt, ...) logger("INFO", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_WARN(fmt, ...) logger("WARNING", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define LOG_ERR(fmt, ...) logger("ERROR", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #else
 #define LOG_INFO(fmt, args...) logger("INFO", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_WARN(fmt, args...) logger("WARNING", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##args)
 #define LOG_ERR(fmt, args...) logger("ERROR", "%s:%d:%s(): " fmt, __FILE__, __LINE__, __func__, ##args)
-#endif
-
 #endif
