756fdac03b30ed4e3f94c141963cd9c01e4cd320
sam <mail@samueljenks.me>
2023-02-19 21:12:28 +1300

    draft: getslice

--- C_part/src/algorithm/aggregating.c
+++ C_part/src/algorithm/aggregating.c
@@ -7,21 +7,27 @@
 
 #include "../chunkmap.h"
 #include "splitting.h"
+#include "../utility/logger.h"
 
-uint64_t getslice(split* input, int startindex, int endindex, int widthstart, int heightstart) {  
+uint64_t getslice(split* input, int startindex, int endindex, int height) {  
     int size = endindex - startindex;
 
     uint64_t output;
     int bitshift = 63;
     int offset = 0;
+
+    if(size >= bitshift) { //size cannot be larger than 64
+        LOG_ERR("ERROR: buffer size is larger than processor architecture");
+        setError();
+    }
     
-    for(int i = 0 ; i < 8; ++i) {
-        uint64t currentbuffer = ((uint64_t)input.nodes[startindex + offset][height].is_boundary) << bitshift);
-        buffers[i] = currentbuffer;
-        --bitshift;
+    for(int i = 0 ; i < size; ++i) { //if size loops 64 times max it will guarantee the correct max amount of loops
+        uint64_t currentbuffer = ((uint64_t)input.nodes[startindex + offset][height].is_boundary) << bitshift);
+        output |= currentbuffer;
+        --bitshift; //bitshift caps out at 0
         ++offset;
     }
-    return
+    return output;
 }
 
 /// @brief returns a 2D array of ints
@@ -35,28 +41,28 @@ uint64_t* OR_64(splits* input, int width, int height) {
     //3. bitwise OR them together
     //4. write to a single chunkmap
     //5. do it for every slice in the current row
-    bool** output = calloc(1, sizeof(bool) * height);
+    bool** output = calloc(1, sizeof(bool*) * height);
 
     for(int y = 0; y < height; ++y) {
         int startindex = 0;
-        int endindex = 0;
+        int endindex = startindex + 64;
         int widthindex = width - 1;
         output[y] = calloc(1, sizeof(bool) * width);
 
-        while(endindex < width) {   
-            endindex = endindex + 64;
+        while(endindex < width) {
+            endindex = endindex + 63;
 
             if(endindex >= widthindex) {
                 endindex = widthindex;
             }
-            uint64_t split1slice = getslice(input.splits[0], startindex, endindex);
-            uint64_t split2slice = getslice(input.splits[1], startindex, endindex);
-            uint64_t split3slice = getslice(input.splits[2], startindex, endindex);
-            uint64_t split4slice = getslice(input.splits[3], startindex, endindex);
-            uint64_t split5slice = getslice(input.splits[4], startindex, endindex);
-            uint64_t split6slice = getslice(input.splits[5], startindex, endindex);
-            uint64_t split7slice = getslice(input.splits[6], startindex, endindex);
-            uint64_t split8slice = getslice(input.splits[7], startindex, endindex);
+            uint64_t split1slice = getslice(input.splits[0], startindex, endindex, height);
+            uint64_t split2slice = getslice(input.splits[1], startindex, endindex, height);
+            uint64_t split3slice = getslice(input.splits[2], startindex, endindex, height);
+            uint64_t split4slice = getslice(input.splits[3], startindex, endindex, height);
+            uint64_t split5slice = getslice(input.splits[4], startindex, endindex, height);
+            uint64_t split6slice = getslice(input.splits[5], startindex, endindex, height);
+            uint64_t split7slice = getslice(input.splits[6], startindex, endindex, height);
+            uint64_t split8slice = getslice(input.splits[7], startindex, endindex, height);
             uint64_t aggregate = split1slice | split2slice | split3slice | split4slice | split5slice | split6slice | split7slice | split8slice;
 
             startindex = endindex + 1;
