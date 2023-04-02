cfd81cce16a16f0d50d23ca44f495baf9a4e0d9e
doublecouponday <mail@samueljenks.me>
2023-04-03 07:13:59 +1200

    rm: rollback failing positional buildtype

--- build.bat
+++ build.bat
@@ -1,15 +1,4 @@
 @echo off
-::disables echoing so that good looking outputs make it through to std out
-
-if "%1"=="" (
-    echo "Error: No arguments provided."
-    echo "Usage: ./build.sh <buildtype>"
-    echo "buildtype can be Debug or Release"
-    exit 1
-)
-
-set buildtype=%1
-echo "building type: %buildtype%"
 
 cd ../
 git clone https://github.com/madler/zlib.git
@@ -24,55 +13,55 @@ echo "Building zlib..."
 cd zlib
 git checkout v1.2.12
 cmake -B build -G "Visual Studio 17 2022" -D CMAKE_INSTALL_PREFIX="install"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build
 
 echo "Building libpng..."
 cd ../libpng
 git checkout v1.6.35
 cmake -B build -G "Visual Studio 17 2022" -D ZLIB_ROOT="../zlib/install"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build --prefix png-install
 
 echo "Building nanosvg..."
 cd ../nanosvg
 git checkout 3bcdf2f3cdc1bf9197c2dce81368bfc6f99205a7
 cmake -B build -G "Visual Studio 17 2022"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build --prefix install
 
 echo "Building freeglut..."
 cd ../freeglut
 git checkout v3.2.2
 cmake -B build -G "Visual Studio 17 2022"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build --prefix install
 
 echo "Building lunasvg..."
 cd ../lunasvg
 git checkout v2.3.1
 cmake -B build -G "Visual Studio 17 2022" -D CMAKE_INSTALL_PREFIX="install"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build --prefix install
 
 echo "Building libjpeg-turbo..."
 cd ../libjpeg
 git checkout libjpeg-turbo-2.1.0
 cmake -B build -G "Visual Studio 17 2022"
-cmake --build build -j4 --config %buildtype%
+cmake --build build -j4 --config Release
 cmake --install build --prefix install
 
 @REM echo "Building opencl..."
 @REM cd ../OpenCL-SDK
 @REM git checkout v2022.09.30
 @REM cmake -G "Visual Studio 17 2022" -D CMAKE_INSTALL_PREFIX=install -B build -S ./
-@REM cmake --build build -j4 --config %buildtype% --target install -- /m /v:minimal
+@REM cmake --build build -j4 --config Release --target install -- /m /v:minimal
 @REM cmake --install build --prefix install
 
 echo "building Chunkus..."
 cd ../Chunkus
-cmake -B build -G "Visual Studio 17 2022" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="%buildtype%" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
-cmake --build build -j4 --config %buildtype%
+cmake -B build -G "Visual Studio 17 2022" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE=Release -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
+cmake --build build -j4 --config Release
 cmake --install build --prefix install
 
 cd ./Rust_part
