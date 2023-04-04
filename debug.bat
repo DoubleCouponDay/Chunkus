2ff43e921d93d61281eb703c61c7a325e638c3bb
doublecouponday <mail@samueljenks.me>
2023-04-05 06:40:45 +1200

    Fix: Debug build failing

--- debug.bat
+++ debug.bat
@@ -1,10 +1,20 @@
-echo on
+@echo off
+
+cd ../
+git clone https://github.com/FreeGLUTProject/freeglut.git
+
+echo "Building freeglut..."
+cd ./freeglut/
+git checkout v3.2.2
+cmake -B build -G "Visual Studio 17 2022"
+cmake --build build -j4 --config Debug
+cmake --install build --prefix install --config Debug
 
 echo "building Chunkus..."
 cd ../Chunkus
-cmake -B build -G "Visual Studio 17 2022" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE="Debug" -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
+cmake -B build -G "Visual Studio 17 2022" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -D CMAKE_INSTALL_PREFIX="install" -D CMAKE_BUILD_TYPE=Debug -D CMAKE_C_COMPILER="gcc" -D CMAKE_EXPORT_COMPILE_COMMANDS="ON"
 cmake --build build -j4 --config Debug
-cmake --install build --prefix install
+cmake --install build --prefix install --config Debug
 
 cd ./Rust_part
 cargo build
